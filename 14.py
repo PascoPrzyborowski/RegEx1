4b0843ebd4ad36aafd2c88b5e92529494df18147
PascoPrzyborowski <unity01@gmx.net>
2022-09-13 17:54:03 +0200

    regex1

---
+++ 14.py
@@ -0,0 +1,18 @@
+# ## Input/Output:
+
+# ```
+# "0023.07623070"   -->   "23.0762307"  
+# "hello world"     -->   "hello world"  
+# "01230"           -->   "1230"  
+# ```
+
+
+
+def del_trailing(text):
+    pattern = r""
+    print(text)
+
+
+text1 = "0023.07623070"
+text2 = "hello world"
+text3 = "01230"
